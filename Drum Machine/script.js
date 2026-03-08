1a783fcf19b3948fcd8c9a54002b2e60cd61785a
Aj.codes <ajifolao@gmail.com>
2026-03-09 00:52:21 +0100

    Drum Machine updated

--- Drum Machine/script.js
+++ Drum Machine/script.js
@@ -64,6 +64,44 @@ document.addEventListener("keydown", function (e) {
     if (e.key === audio.id.toLowerCase()) {
       audio.play();
       display.textContent = audio.parentElement.id.toUpperCase();
+      if (e.key === "q") {
+        audio.parentElement.style.backgroundColor = "crimson";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "w") {
+        audio.parentElement.style.backgroundColor = "blue";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "e") {
+        audio.parentElement.style.backgroundColor = "#3d402f";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "a") {
+        audio.parentElement.style.backgroundColor = "green";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "s") {
+        audio.parentElement.style.backgroundColor = "brown";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "d") {
+        audio.parentElement.style.backgroundColor = "indigo";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "z") {
+        audio.parentElement.style.backgroundColor = "violet";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "x") {
+        audio.parentElement.style.backgroundColor = "#7aa6b3";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      } else if (e.key === "c") {
+        audio.parentElement.style.backgroundColor = "orange";
+        audio.parentElement.style.color = "white";
+        audio.parentElement.style.border = "2px solid black";
+      }
+      changeColor(audio.parentElement);
     } else {
       return;
     }
